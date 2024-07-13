d58d4a3263749e762943f6c2a16bd91487856d42
Eric Gibertson <eric_gilbertson@yahoo.com>
2024-07-12 20:04:34 -0700

    update playlist with disk title

--- make_playlist.py
+++ make_playlist.py
@@ -8,6 +8,32 @@ from urllib import request, parse
 
 playlist_file_name = None
 
+def selectTitle(artist, track, titleAr):
+    titleCnt = len(titleAr)
+    if titleCnt == 0:
+        return None
+
+    print("Select release for {} - {}: ".format(artist, track))
+    idx = 0
+    for title in titleAr:
+        print('{} - {}'.format(idx, title))
+        idx = idx + 1
+
+    diskNumStr = input('Disk: ')
+    if (len(diskNumStr) == 0):
+        if titleCnt == 1:
+            diskNumStr = "0"
+        else:
+            return None
+
+    diskNum = int(diskNumStr)
+    if 0 <= diskNum < len(titleAr):
+        title = titleAr[diskNum].split('\t')[1].strip()
+        return title
+    else:
+        print("Invalid value, try again")
+        return(selectTitle(titleAr))
+
 def findDiskTitle(artist, track):
     #recording:It's five o'clock && artist:Aphrodite's Child&fmt=json&inc=
     query = 'recording:"{}" && artist:{}'.format(track, artist)
@@ -20,14 +46,22 @@ def findDiskTitle(artist, track):
 
     if response.status != 200:
         print("Musicbrainz error: {}".format(response.status))
-        return null
+        return Null
 
     result = json.loads(response.read())
+    titles = []
     for recording in result['recordings']:
         artistName = recording['artist-credit'][0]['name']
         release = recording['releases'][0]
         releaseTitle = release['title']
-        print("Found: {}, {}, {}, {}".format(recording['title'], artistName, releaseTitle, recording['score']))
+        score = int(recording['score'])
+        #print("Found: {}, {}, {}, {}".format(recording['title'], artistName, releaseTitle, score))
+        if (artist.find(artistName) >= 0)  & (int(score) > 90):
+            titles.append('{} -\t {}'.format(artistName, releaseTitle))
+
+    title = selectTitle(artist, track, titles)
+    return title
+
 
 def parse_args(argv):
    global playlist_file_name
@@ -56,17 +90,29 @@ if __name__ == '__main__':
     playlist_file = open(playlist_file_name)
     lines = playlist_file.readlines()
 
+    result = ''
+    isDirty = False
     for line in lines:
         fieldAr = line.split('\t')
         fieldCnt = len(fieldAr)
-        if fieldCnt != 2 or line[0] == '#':
-            continue
+        if (fieldCnt == 2) & (line[0] != '#'):
+            artist = fieldAr[0].strip()
+            track  = fieldAr[1].strip()
+            release = findDiskTitle(artist, track)
+            if release != None:
+                line = '{}\t{}\t{}\n'.format(artist, track, release)
+                isDirty = True
 
-        print("line: " + line)
+        result = result + line
 
-        artist = fieldAr[0]
-        track  = fieldAr[1]
-        findDiskTitle(artist, track)
+    new_file_name = playlist_file_name + '_new'
+    new_file = open(new_file_name, 'w')
+    new_file.write(result)
+    new_file.close()
+
+
+
+
 
 
 
