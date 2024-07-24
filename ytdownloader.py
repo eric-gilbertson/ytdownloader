a87d98b64fc2fa9668595f32c4649bb1b09b9b30
Eric Gilbertson <eric_gilbertson@yahoo.com>
2024-07-24 15:56:08 -0700

    add ability to play track by double clicking it

--- ytdownloader.py
+++ ytdownloader.py
@@ -16,6 +16,7 @@ import tkinter.font as tkFont
 import tkinter.ttk as ttk
 from tkinterdnd2 import *
 from audio_trimmer import trim_audio
+import pyaudio, wave
 
 DOWNLOAD_DIR = expanduser("~") + "/Music/ytdl"
 YTDL_PATH = None
@@ -27,11 +28,48 @@ def logit(msg):
         logfile.write(timestr + msg + '\n')
 
 
+class PlayerThread(threading.Thread):
+    def __init__(self, idx, filelist):
+        super(PlayerThread, self).__init__()
+        self.is_playing = False
+        self.stop = False
+        self.filelist = filelist
+        self.idx = idx
+
+    def stop_player(self):
+        self.stop = True
+
+    def run(self):
+        while not self.stop and self.idx < len(self.filelist):
+            filename = self.filelist[self.idx]
+            self.idx = self.idx + 1
+            self.is_playing = True
+            with wave.open(filename, 'rb') as wf:
+                # Instantiate PyAudio and initialize PortAudio system resources (1)
+                p = pyaudio.PyAudio()
+    
+                # Open stream (2)
+                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                                channels=wf.getnchannels(),
+                                rate=wf.getframerate(),
+                                output=True)
+    
+                # Play samples from the wave file (3)
+                while not self.stop and len(data := wf.readframes(1024)):
+                    stream.write(data)
+    
+                # Close stream (4)
+                stream.close()
+    
+            # Release PortAudio system resources (5)
+            p.terminate()
+
+            self.is_playing = False
+
 # TODO
 # escape quotes in downloaded filename when invoking ffmpeg trim.
 # set input box red/green upon completion
 # remove '(Official Video)' and '(Lyric Video)' and single quotes from filename.
-
 class CommandThread(threading.Thread):
     def __init__(self, cmd):
         super(CommandThread, self).__init__()
@@ -50,17 +88,46 @@ def schedule_check(command_thread):
 
 def clean_filepath(filepath):
     new_file = filepath
-    if new_file.find('\'') >= 0:
-        new_file = new_file.replace('\'', '')
 
-    idx1 = new_file.find(' (')
+    # remove parentheticals first
+    idx1 = new_file.find('(')
     if idx1 > 0:
         idx2 = new_file.find(')', idx1)
         if idx2 > idx1:
             new_file = new_file[0:idx1] + new_file[idx2 + 1:]
 
-    if new_file.find('/NA_') >= 0:
-        new_file = new_file.replace('/NA_', '/')
+    idx1 = new_file.find('[')
+    if idx1 > 0:
+        idx2 = new_file.find(']', idx1)
+        if idx2 > idx1:
+            new_file = new_file[0:idx1] + new_file[idx2 + 1:]
+
+
+    if new_file.find('\'') >= 0:
+        new_file = new_file.replace('\'', '')
+
+
+    if new_file.find('｜') >= 0:
+        new_file = new_file.replace('｜', '\t')
+
+    if new_file.find('＂') >= 0:  # special fat double quote from &quot; in html
+        new_file = new_file.replace('＂', '')
+
+    if new_file.find('"') >= 0:  # regular double quote
+        new_file = new_file.replace('"', '')
+
+    if new_file.find('-') >= 0 or new_file.find('–') >= 0:
+        new_file = new_file.replace('-', '\t')
+
+
+    if new_file.find(' Official HD Audio') >= 0:  # regular double quote
+        new_file = new_file.replace(' Official HD Audio', '')
+
+    if new_file.find(' Official Music Video') >= 0:  # regular double quote
+        new_file = new_file.replace(' Official Music Video', '')
+
+    if new_file.find('NA_') >= 0:
+        new_file = new_file.replace('NA_', '')
 
     # just in case we lost the suffix in the transform.
     if not new_file.endswith('.wav'):
@@ -82,18 +149,18 @@ def check_if_done(command_thread):
         errmsg = str(command_thread.stderr) # returns '\\\' even when there is no error.
         length = len(errmsg)
 
-        # ignore 403 errors because they are usually false.
-        is403 = errmsg.find(' 403') > 0
-        if is403:
-            tk.messagebox.showwarning(title='403 Error', message=errmsg)
+        ignoreMsg = ((errmsg.find('Skipping player responses from android') > 0) | \
+                     (errmsg.find('You may experience throttling for some formats') > 0) | \
+                     (errmsg.find(' 403') > 0))
 
-        if (command_thread.process.returncode == 0 or is403) and len(errmsg) < 4:
+        if (command_thread.process.returncode == 0 and (ignoreMsg or len(errmsg) < 4)):
             res = str(command_thread.stdout, 'utf-8')
             idx1 = res.rfind("Destination: ") + 13
             idx2 = res.find(".wav", idx1)
             if idx1 > 13 and idx2 > idx1:
                 filepath =  res[idx1:idx2+4]
                 filepath = clean_filepath(filepath)
+                logit("Add file: " + filepath)
                 trim_audio(filepath)
 
                 listbox.populate_list()
@@ -104,7 +171,7 @@ def check_if_done(command_thread):
             else:
                 errmsg = res
 
-        if len(errmsg) > 4:
+        if len(errmsg) > 4 and not ignoreMsg:
             tk.messagebox.showwarning(title='Error', message=errmsg)
             #control_panel.url.config({"background": "Red"})
             #control_panel.url.update()
@@ -133,6 +200,9 @@ def delete_file_after_delay(file_name):
 
 class ControlPanel(object):
     def __init__(self, frame):
+        self.player = None
+        self.list_widget = None
+
         top_frame = tk.Frame(master=root, height=30)
         lbl = tk.Label(master=top_frame, text='URL:')
         lbl.place(x=0, y=1)
@@ -141,10 +211,15 @@ class ControlPanel(object):
         self.url.bind('<Return>', self._on_url_enter)
         self.url.place(x=30, y=0)
         button = tk.Button(master=top_frame, command= self._fetch_url, text="Fetch", width=5, fg="black", )
-        button.place(x=400, y=0)
+        button.place(x=370, y=0)
         button = tk.Button(master=top_frame, command= self._reload, text="Reload", width=5, fg="black", )
-        button.place(x=480, y=0)
+        button.place(x=450, y=0)
+        button = tk.Button(master=top_frame, command= self._play_file, text="Play", width=5, fg="black", )
+        button.place(x=530, y=0)
+        button = tk.Button(master=top_frame, command= self._stop_play, text="Stop", width=5, fg="black", )
+        button.place(x=610, y=0)
         top_frame.pack(fill=tk.X)
+
 
     def _on_url_enter(self, widget):
         #logit("enter: " + self.url.get())
@@ -152,9 +227,32 @@ class ControlPanel(object):
 
     def set_list_widget(self, list_widget):
         self.list_widget = list_widget
+        self.list_widget.tree.bind('<Double-1>', self.double_click)
+
 
     def _reload(self):
         self.list_widget.reload_list();
+
+    def _play_file(self):
+        list = self.list_widget.tree.get_children()
+        item_name = self.list_widget.tree.focus()
+        idx = self.list_widget.tree.index(item_name)
+        print("Play: " + item_name)
+        if self.player:
+            self.player.stop_player()
+
+        self.player = PlayerThread(idx, list)
+        self.player.start()
+
+    def double_click(self, event):
+        item = self.list_widget.tree.focus()
+        print("double: {}".format(item))
+        #self._play_file()
+
+    def _stop_play(self):
+        item_name = self.list_widget.tree.selection()
+        print("stop play")
+        self.player.stop_player()
 
     def _fetch_url(self):
         #control_panel.url.config({"background": "White"})
@@ -168,7 +266,7 @@ class ControlPanel(object):
             cmd = YTDL_PATH + ' --extract-audio --audio-format wav -o {} {}'.format(out_file, self.urlEntry.get())
             logit("cmd: " + cmd)
             download_thread = CommandThread(cmd)
-            control_panel.url.config(cursor="wait")
+            control_panel.url.config(cursor="clock")
             control_panel.url.update()
             download_thread.start()
             schedule_check(download_thread)
@@ -181,11 +279,17 @@ class FilePickerListbox(object):
         self.item_name = None
         self._setup_widgets(frame)
         self._build_tree()
+        self._set_file_header()
 
         self.tree.drag_source_register(1, DND_FILES)
         self.tree.dnd_bind('<<DragInitCmd>>', self.drag_init)
         self.tree.dnd_bind('<<DragEndCmd>>', self.drag_end)
         self.tree.dnd_bind('<<DragDataGetCmd>>', self.drag_data_get)
+
+    def _set_file_header(self):
+        msg = 'Files: ({})'.format(len(self.tree.get_children()))
+        self.tree.heading('File', text=msg)
+
 
     def drag_init(self, event):
         data = ()
@@ -206,7 +310,7 @@ class FilePickerListbox(object):
         file_name = self.item_name[0]
         #logit("drag end:" + file_name)
         # Don't delte LID files since they are reused.
-        if file_name.find("/LID_") < 0:
+        if False and file_name.find("/LID_") < 0:
             self.tree.delete(file_name)
             threading.Thread(target=delete_file_after_delay, args=([file_name])).start()
             self.tree.dragging = False
@@ -219,7 +323,7 @@ class FilePickerListbox(object):
         container.pack(fill='both', expand=True)
 
         # create a treeview with dual scrollbars
-        self.tree = ttk.Treeview(columns=self.table_header, height=500, show="headings")
+        self.tree = ttk.Treeview(columns=self.table_header, height=520, show="headings")
         vsb = ttk.Scrollbar(orient="vertical", command=self.tree.yview)
         hsb = ttk.Scrollbar(orient="horizontal", command=self.tree.xview)
         self.tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
@@ -271,6 +375,8 @@ class FilePickerListbox(object):
             else:
                 pass #logit("File already exists: " + name)
 
+        self._set_file_header()
+
 
     def _build_tree(self):
         for col in self.table_header:
@@ -286,11 +392,11 @@ def resize(event):
 if __name__ == '__main__':
     root = TkinterDnD.Tk()
     root.title("Youtube Download Tool")
-    root.geometry("560x480")
+    root.geometry("560x490")
     control_panel = ControlPanel(root)
 
-    YTDL_PATH = spawn.find_executable('youtube-dl')
-    YTDL_PATH = "/Users/Barbara/src/youtube-dl/youtube-dl"
+    YTDL_PATH = spawn.find_executable('yt-dlp')
+    #YTDL_PATH = "/Users/Barbara/src/youtube-dl/youtube-dl"
 
     logit("ytdl path: " + YTDL_PATH)
 
