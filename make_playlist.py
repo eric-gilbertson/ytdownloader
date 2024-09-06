bfe424db6c68ea36aec372b99b70d43f2b193053
Eric Gilbertson <eric_gilbertson@yahoo.com>
2024-09-05 17:31:01 -0700

    make disk title check CI

--- make_playlist.py
+++ make_playlist.py
@@ -1,9 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 # this file checks the show archvie for the files listed in the input file
 # specified with the -f argument. show length is 1 hour and it can be 
 # overridden with the -l argument.
 #
-import os, getopt, sys, http.client, json, glob, urllib
+import os, getopt, sys, http.client, json, glob, urllib, time
 from urllib import request, parse
 
 playlist_file_name = None
@@ -35,31 +35,46 @@ def selectTitle(artist, track, titleAr):
         return(selectTitle(titleAr))
 
 def findDiskTitle(artist, track):
+    #print("Find: {} - {}".format(artist, track))
     #recording:It's five o'clock && artist:Aphrodite's Child&fmt=json&inc=
     query = 'recording:"{}" && artist:{}'.format(track, artist)
-    url = 'https://musicbrainz.org/ws/2/recording/?limit=20&query=' + urllib.parse.quote_plus(query)
+    url = 'https://musicbrainz.org/ws/2/recording/?limit=40&query=' + urllib.parse.quote_plus(query)
     req = request.Request(url, method='GET')
     req.add_header("Accept", "application/json")
 
+    #print("url: {}".format(url))
     response = request.urlopen(req)
-    print("Status: {} and reason: {}".format(response.status, response.reason))
+    #print("Status: {} and reason: {}".format(response.status, response.reason))
 
     if response.status != 200:
         print("Musicbrainz error: {}".format(response.status))
-        return Null
+        return None
 
     result = json.loads(response.read())
-    titles = []
-    for recording in result['recordings']:
-        artistName = recording['artist-credit'][0]['name']
-        release = recording['releases'][0]
-        releaseTitle = release['title']
-        score = int(recording['score'])
-        #print("Found: {}, {}, {}, {}".format(recording['title'], artistName, releaseTitle, score))
-        if (artist.find(artistName) >= 0)  & (int(score) > 90):
-            titles.append('{} -\t {}'.format(artistName, releaseTitle))
+    if result['count'] == 0:
+        print("No results for: {} - {}".format(artist, track))
 
-    title = selectTitle(artist, track, titles)
+    choices = []
+    releases = []
+    lastReleaseTitle = None
+    for recording in result['recordings']:
+        artistName = recording['artist-credit'][0]['name'].lower()
+        for release in recording['releases']:
+            releaseTitle = release['title']
+            score = int(recording['score'])
+            # protect against missing status field
+            isOfficial = release.get('status', 'Official') == 'Official'
+            #print("Found: {}, {}, {}, {}".format(recording['title'], artistName, releaseTitle, score))
+            isNew = not releaseTitle.lower() in releases
+            if isOfficial & (artist.lower().find(artistName) >= 0) & (int(score) > 90) & isNew:
+                choices.append('{} -\t {}'.format(artistName, releaseTitle))
+                releases.append(releaseTitle.lower())
+                lastReleaseTitle = releaseTitle
+
+    title = lastReleaseTitle
+    if len(choices) > 1:
+        title = selectTitle(artist, track, choices)
+
     return title
 
 
@@ -83,7 +98,9 @@ if __name__ == '__main__':
     #global playlist_file_name
 
     parse_args(sys.argv[1:])
-    if playlist_file_name is None or not os.path.exists(playlist_file_name):
+    if len(sys.argv) <= 1:
+        print("Usage: {} <PLAYLIST_FILE>".format(sys.argv[0]))
+    elif playlist_file_name is None or not os.path.exists(playlist_file_name):
         print("Error: input file -{}- does not exist.".format(playlist_file_name))
         sys.exit(1)
 
@@ -93,13 +110,22 @@ if __name__ == '__main__':
     result = ''
     isDirty = False
     for line in lines:
+        if line[0] == '#' or line.find("break") == 0:
+            result = result + line
+            continue
+          
         fieldAr = line.split('\t')
         fieldCnt = len(fieldAr)
-        if (fieldCnt == 2) & (line[0] != '#'):
+
+        time.sleep(1)
+
+        if fieldCnt < 2:
+            print("Invalid line: {}".format(line))
+        elif fieldCnt == 2 or (fieldCnt > 2 and len(fieldAr[2].strip()) == 0):
             artist = fieldAr[0].strip()
-            track  = fieldAr[1].strip()
+            track = fieldAr[1].strip()
             release = findDiskTitle(artist, track)
-            if release != None:
+            if release is not None:
                 line = '{}\t{}\t{}\n'.format(artist, track, release)
                 isDirty = True
 
